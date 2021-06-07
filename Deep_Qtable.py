e6abb54551782e3c5a5c1e642c9895dcabf0fbb9
ShihYT <gaorda310130@gmail.com>
2021-06-07 14:43:56 +0800

    Update Deep_Qtable.py

--- Deep_Qtable.py
+++ Deep_Qtable.py
@@ -96,85 +96,59 @@ class DQN(object):
         loss.backward()
         self.optimizer.step()
 
-dqn = DQN() # 定义 DQN 系统
-
-# -- Configuration variables --
-# Edit these variables to configure the simulator
-# Change which set of video trace to use: AsianCup_China_Uzbekistan, Fengtimo_2018_11_3, game, room, sports, YYF_2018_08_12
-VIDEO_TRACE = testcase[0]
-
-# Change which set of network trace to use: 'fixed' 'low' 'medium' 'high'
-NETWORK_TRACE = testcase[1]
-
-# Turn on and off logging.  Set to 'True' to create log files.
-# Set to 'False' would speed up the simulator.
-DEBUG = testcase[2]
-
-# Control the subdirectory where log files will be stored.
-LOG_FILE_PATH = './log/'
+def training(training_set):
+    VIDEO_TRACE = testcase[0]
+    NETWORK_TRACE = testcase[1]
+    DEBUG = testcase[2]
+    LOG_FILE_PATH = './log/'
+    if not os.path.exists(LOG_FILE_PATH):
+        os.makedirs(LOG_FILE_PATH)
+    print(f"{VIDEO_TRACE},{NETWORK_TRACE}: Start")
     
-# create result directory
-if not os.path.exists(LOG_FILE_PATH):
-    os.makedirs(LOG_FILE_PATH)
+    # -- End Configuration --
+    # You shouldn't need to change the rest of the code here.
+    network_trace_dir = './dataset/network_trace/' + NETWORK_TRACE + '/'
+    video_trace_prefix = './dataset/video_trace/' + VIDEO_TRACE + '/frame_trace_'
+    all_cooked_time, all_cooked_bw, all_file_names = load_trace.load_trace(network_trace_dir)
+    #random_seed 
+    random_seed = 2
+    count = 0
+    trace_count = 1
+    FPS = 25
+    frame_time_len = 0.04
+    reward_all_sum = 0
+    run_time = 0
+    net_env = fixed_env.Environment(all_cooked_time=all_cooked_time, all_cooked_bw=all_cooked_bw,
+                                    random_seed=random_seed, logfile_path=LOG_FILE_PATH,
+                                    VIDEO_SIZE_FILE=video_trace_prefix, Debug = DEBUG)
 
-print(f"{VIDEO_TRACE},{NETWORK_TRACE}: Start")
-# -- End Configuration --
-# You shouldn't need to change the rest of the code here.
+    BIT_RATE      = [500.0,850.0,1200.0,1850.0] # kpbs
+    TARGET_BUFFER = [0.5,1.0]   # seconds
+    # ABR setting
+    RESEVOIR = 0.5
+    CUSHION  = 2
 
-network_trace_dir = './dataset/network_trace/' + NETWORK_TRACE + '/'
-video_trace_prefix = './dataset/video_trace/' + VIDEO_TRACE + '/frame_trace_'
+    cnt = 0
+    # defalut setting
+    last_bit_rate = 0
+    bit_rate = 0
+    target_buffer = 0
+    latency_limit = 4
 
-# load the trace
-all_cooked_time, all_cooked_bw, all_file_names = load_trace.load_trace(network_trace_dir)
-#random_seed 
-random_seed = 2
-count = 0
-trace_count = 1
-FPS = 25
-frame_time_len = 0.04
-reward_all_sum = 0
-run_time = 0
-net_env = fixed_env.Environment(all_cooked_time=all_cooked_time, all_cooked_bw=all_cooked_bw,
-                                random_seed=random_seed, logfile_path=LOG_FILE_PATH,
-                                VIDEO_SIZE_FILE=video_trace_prefix, Debug = DEBUG)
-
-BIT_RATE      = [500.0,850.0,1200.0,1850.0] # kpbs
-TARGET_BUFFER = [0.5,1.0]   # seconds
-# ABR setting
-RESEVOIR = 0.5
-CUSHION  = 2
-
-cnt = 0
-# defalut setting
-last_bit_rate = 0
-bit_rate = 0
-target_buffer = 0
-latency_limit = 4
-
-# QOE setting
-reward_frame = 0
-reward_all = 0
-SMOOTH_PENALTY= 0.02
-REBUF_PENALTY = 1.85
-LANTENCY_PENALTY = 0.005
-SKIP_PENALTY = 0.5
-
-for i_episode in range(400):
-    s = env.reset()
+    # QOE setting
+    reward_frame = 0
+    reward_all = 0
+    SMOOTH_PENALTY= 0.02
+    REBUF_PENALTY = 1.85
+    LANTENCY_PENALTY = 0.005
+    SKIP_PENALTY = 0.5
+    
     while True:
-        env.render()    # 显示实验动画
-        a = dqn.choose_action(time,time_interval, send_data_size, chunk_len,rebuf, buffer_size, play_time_len,end_delay,\
-            cdn_newest_id, download_id, cdn_has_frame,skip_frame_time_len, decision_flag,\
-            buffer_flag, cdn_flag, skip_flag,end_of_video)
-
+        reward = 0
         # 选动作, 得到环境反馈
-        biterate = int(a/20);
-        replay_buffer = a%2
-        latency_limit = int((a%20)/2)/10
         next_time, next_time_interval, next_send_data_size, next_chunk_len, next_rebuf, next_buffer_size, next_play_time_len, next_end_delay,\
             next_cdn_newest_id, next_download_id, next_cdn_has_frame, next_skip_frame_time_len, next_decision_flag,\
             next_buffer_flag, next_cdn_flag, next_skip_flag, next_end_of_video = net_env.get_video_frame(bit_rate,target_buffer, latency_limit)
-
         # QOE setting 
         if end_delay <=1.0:
             LANTENCY_PENALTY = 0.005
@@ -188,6 +162,56 @@ for i_episode in range(400):
         if decision_flag or end_of_video:
             # reward formate = play_time * BIT_RATE - 4.3 * rebuf - 1.2 * end_delay
             reward += -1 * SMOOTH_PENALTY * (abs(BIT_RATE[bit_rate] - BIT_RATE[last_bit_rate]) / 1000)
+            last_bit_rate = bit_rate
+            cnt += 1
+            timestamp_start = tm.time()
+            a = dqn.choose_action(time,time_interval, send_data_size, chunk_len,rebuf, buffer_size, play_time_len,end_delay,\
+            cdn_newest_id, download_id, cdn_has_frame,skip_frame_time_len, decision_flag,\
+            buffer_flag, cdn_flag, skip_flag,end_of_video)
+            bit_rate = int(a/20);
+            replay_buffer = a%2
+            latency_limit = int((a%20)/2)/10
+            timestamp_end = tm.time()
+            call_time_sum += timestamp_end - timestamp_start
+        
+        
+        dqn.store_transition([time, time_interval, send_data_size, chunk_len,rebuf, buffer_size, play_time_len,end_delay,\
+            cdn_newest_id, download_id, cdn_has_frame,skip_frame_time_len, decision_flag,\
+            buffer_flag, cdn_flag, skip_flag,end_of_video], a, reward, [next_time, next_time_interval, next_send_data_size, next_chunk_len, next_rebuf, next_buffer_size, next_play_time_len, next_end_delay,\
+            next_cdn_newest_id, next_download_id, next_cdn_has_frame, next_skip_frame_time_len, next_decision_flag,\
+            next_buffer_flag, next_cdn_flag, next_skip_flag, next_end_of_video])
+        if dqn.memory_counter > MEMORY_CAPACITY:
+            dqn.learn() # 记忆库满了就进行学习
+
+        if done:    # 如果回合结束, 进入下回合
+            break
+            
+#         time = next_time
+#         time_interval = next_time_interval
+#         send_data_size = next_send_data_size
+#         chunk_len = next_chunk_len
+#         rebuf = next_rebuf
+#         buffer_size = next_buffer_size
+#         play_time_len = next_play_time_len
+#         end_delay = next_end_delay
+#         cdn_newest_id = next_cdn_newest_id
+#         download_id = next_download_id
+#         cdn_has_frame,next_
+#         skip_frame_time_len, next_
+#         decision_flag next_
+            
+#         buffer_flag next_
+#         cdn_flag next_ cdn_flag
+#         kip_flag next_kip_flag
+#         end_of_video next_end_of_video
+
+dqn = DQN() # 定义 DQN 系统
+
+for i_episode in range(400):
+    s = env.reset()
+    while True:
+        r
+        
         
         # 修改 reward, 使 DQN 快速学习
         x, x_dot, theta, theta_dot = s_
